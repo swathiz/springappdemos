c180c0fd6130e98b4c715d465989ff7df3cb9cee
SWATHI <swathi@specrio.co>
2022-08-17 10:04:52 +0530

    spring changes

--- S01springcoredi/src/main/java/com/samples/S01springcoredi/App.java
+++ S01springcoredi/src/main/java/com/samples/S01springcoredi/App.java
@@ -1,13 +1,32 @@
 package com.samples.S01springcoredi;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+       ClassPathXmlApplicationContext springContainer = new ClassPathXmlApplicationContext("com/samples/S01springcoredi/SpringConfig.xml");
+       
+//       Employee emp = (Employee) springContainer.getBean("emp"); 
+//       System.out.println(emp);
+       
+       
+//       Employee em1 = new Employee();
+//       em1.setId(10);
+//       em1.setName("x");
+
+//       Hospital hospital = (Hospital) springContainer.getBean("hospital");
+//       System.out.println(hospital);
+       
+       
+//       CarDealer dealer = (CarDealer) springContainer.getBean("cardealer");
+//       System.out.println(dealer);
+       
+       Customer customer = (Customer) springContainer.getBean("customer");
+       System.out.println(customer);
+       
+       ShoppingCart sc = (ShoppingCart) springContainer.getBean("shoppingcart");
+       System.out.println(sc);
     }
 }
---
+++ S01springcoredi/src/main/java/com/samples/S01springcoredi/ShoppingCart.java
@@ -0,0 +1,19 @@
+package com.samples.S01springcoredi;
+
+public class ShoppingCart {
+	private Item item;
+	
+	public Item getItem() {
+		return item;
+	}
+
+	public void setItem(Item item) {
+		this.item = item;
+	}
+
+	@Override
+	public String toString() {
+		return "ShoppingCart [item=" + item + "]";
+	}
+	
+}
